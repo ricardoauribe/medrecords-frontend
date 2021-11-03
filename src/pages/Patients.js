b995013caf3ae7281e7e152a99a1fb544a1a696f
Ricardo Uribe <ricardoauribe@hotmail.com>
2021-11-03 10:14:08 -0600

    Creating Patitents Home

---
+++ src/pages/Patients.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Patients = () => {
+  <>
+    <h1>
+      Patients
+    </h1>
+    <p>
+      Information about current members/patients
+    </p>
+  </>
+}
+
+export default Patients;
