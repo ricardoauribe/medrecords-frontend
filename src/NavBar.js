ed5f32fe01477a8377bee895c6bcbc7b7ae9634d
Ricardo Uribe <ricardoauribe@hotmail.com>
2021-11-12 09:56:19 -0600

    First approach to bootstrap navbar

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,20 +1,20 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
+import { Container, Navbar, Nav } from "react-bootstrap";
 
 const NavBar = () => (
-  <nav>
-    <ul>
-      <li>
-        <Link to="/">Home</Link>
-      </li>
-      <li>
-        <Link to="/patients">Patients</Link>
-      </li>
-      <li>
-        <Link to="/others">Others</Link>
-      </li>
-    </ul>
-  </nav>
+  <>
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="/">Home</Navbar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link href="/">Home</Nav.Link>
+          <Nav.Link href="/patients">Patients</Nav.Link>
+          <Nav.Link href="/others">Others</Nav.Link>
+        </Nav>
+      </Container>
+    </Navbar>
+  </>
 )
 
 export default NavBar;
