a3e6240f8d296f8e26ee374c547f89ea347cfb54
Ricardo Uribe <ricardoauribe@hotmail.com>
2021-11-19 10:14:29 -0600

    Adding images to cards

--- src/components/PatientCard.js
+++ src/components/PatientCard.js
@@ -5,7 +5,7 @@ const PatientCard = ({patient}) => {
   return(
     <>
       <Card style={{ width: '18rem'}}>
-        <Card.Img variant="top"/>
+        <Card.Img variant="top" src={"./avatar-man.png"} />
         <Card.Body>
           <Card.Title>{patient.name} {patient.lastname}</Card.Title>
           <Card.Text>
