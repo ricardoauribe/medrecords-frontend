35f4db552ed455bbd9a0fb5c7567e2eb1d2d9fb0
Ricardo Uribe <ricardoauribe@hotmail.com>
2021-11-19 10:21:14 -0600

    Conditional images according to sex

--- src/components/PatientCard.js
+++ src/components/PatientCard.js
@@ -5,11 +5,18 @@ const PatientCard = ({patient}) => {
   return(
     <>
       <Card style={{ width: '18rem'}}>
-        <Card.Img variant="top" src={"./avatar-man.png"} />
+        {
+          (patient.sex == "male") 
+            ?<Card.Img variant="top" src={"./avatar-man.png"} />
+            :<Card.Img variant="top" src={"./avatar-woman.png"} />
+        }
+        
         <Card.Body>
           <Card.Title>{patient.name} {patient.lastname}</Card.Title>
           <Card.Text>
             {patient.age}
+            <br/>
+            {patient.sex}
           </Card.Text>
         </Card.Body>
       </Card>
