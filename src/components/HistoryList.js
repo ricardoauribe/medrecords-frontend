30c59487b654f3503d22ac3ad674b1aaab25c43a
Ricardo Uribe <ricardoauribe@hotmail.com>
2021-11-24 13:10:27 -0600

    parametrizing patient id

--- src/components/HistoryList.js
+++ src/components/HistoryList.js
@@ -2,18 +2,19 @@ import React, { useEffect, useState } from "react";
 
 const HistoryList = ({patientGUID}) => {
 
+  const id = patientGUID;
   const error = null;
   const [patientHistory, setPatientHistory] = useState([]);
 
   useEffect(() => {
     const fetchHistory = async () => {
-      const result = await fetch(`http://localhost:8000/api/history/AAAAA`)
+      const result = await fetch(`http://localhost:8000/api/history/${id}`)
         .catch(error);
       const body = await result.json();
       setPatientHistory(body);
     }
     fetchHistory();
-  }, [])
+  }, [id])
 
 
   return(
@@ -21,7 +22,7 @@ const HistoryList = ({patientGUID}) => {
       <div>
         This is the history for 
         {patientHistory.map((appointment, key) => (
-          <p>{appointment.appointment_id} | {appointment.date} | {appointment.type} | {appointment.description}  | {appointment.recepieURL} | {appointment.billPDF} | {appointment.billXML}  </p>
+          <p key={key}>{appointment.appointment_id} | {appointment.date} | {appointment.type} | {appointment.description}  | {appointment.recepieURL} | {appointment.billPDF} | {appointment.billXML}  </p>
         ))}
       </div>
       
