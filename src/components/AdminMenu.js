c0095c32f8ee646eed843b8ddceda36e8721a7a0
Ricardo Uribe <ricardoauribe@hotmail.com>
2021-11-30 12:50:26 -0600

    Admin menu component creation

---
+++ src/components/AdminMenu.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AdminMenu = () => {
+  return (
+    <>
+      New Patient | New Appointment
+    </>
+  )
+}
+
+export default AdminMenu;
--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -1,8 +1,10 @@
 import React from "react";
+import AdminMenu from "../components/AdminMenu";
 
 const Admin = () => {
   return(
     <>
+      <AdminMenu />
       <h1>Admin Home</h1>
       <p>TODO: Add a menu with modules to add patients and past medical appointments</p>
     </>
