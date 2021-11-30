88acdfeb96b2e061ce2f2a380724698ed96b2825
Ricardo Uribe <ricardoauribe@hotmail.com>
2021-11-30 08:29:32 -0600

    Adding todos

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,7 +5,18 @@ const Home = () => {
     <>
     <h1>Medical Records Home</h1>
     <div class="bodyText">
-      <p>This web application contains a registry of the family medical records</p>
+      <p>This web application contains a registry of the family medical 
+        records
+      </p>
+      <p>
+        Place statistics on how mane family members, how many appoitments etc.
+      </p>
+      <p>
+        TODO: Create a card to enclose the image
+      </p>
+      <p>
+        TODO: Add a footer
+      </p>
       <img src={"./medBanner.png"} />
     </div>
     
