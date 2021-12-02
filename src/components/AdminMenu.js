871a6e794f9dc502aac5ed75de5e4113cd79974a
Ricardo Uribe <ricardoauribe@hotmail.com>
2021-12-02 10:37:56 -0600

    Identation and todo

--- src/components/AdminMenu.js
+++ src/components/AdminMenu.js
@@ -4,14 +4,14 @@ import { Container, Nav, Navbar } from "react-bootstrap";
 const AdminMenu = () => {
   return (
     <>
-    <Navbar bg="dark" variant="dark" size="sm">
-      <Container>
-        <Nav>
-          <Nav.Link href="/createPatient">New Patient</Nav.Link>
-          <Nav.Link href="/createAppointment">New Appointment</Nav.Link>
-        </Nav>
-      </Container>
-    </Navbar>
+      <Navbar bg="dark" variant="dark" size="sm">
+        <Container>
+          <Nav>
+            <Nav.Link href="/createPatient">New Patient</Nav.Link>
+            <Nav.Link href="/createAppointment">New Appointment</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
     </>
   )
 }
--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -6,6 +6,8 @@ const Admin = () => {
     <>
       <AdminMenu />
       <h1>Admin Home</h1>
+
+      <p>TODO: This page will contain the CRUD for appointments and patients</p>
       <p>TODO: Add a menu with modules to add patients and past medical appointments</p>
     </>
   )
